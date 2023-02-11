4f6f81f56e3b1cf3c389e9359000cc48cf40375c
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2023-02-11 04:02:03 -0900

    update the server code

--- backhend/server.js
+++ backhend/server.js
@@ -4,17 +4,18 @@ const color = require("colors");
 const dotenv = require("dotenv").config();
 const fs = require("fs");
 const cors = require('cors')
-
 PORT = process.env.PORT || 8000;
+
 //middleware
 const { errorHandler } = require("../backhend/middleWare/errorMiddleware");
 //app create
 const app = express();
+
 //app use
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
-app.use("testrig.onrender.com/api/project", require("../backhend/Routes/projectRoute"));
+app.use('/api/project' ,require("../backhend/Routes/projectRoute"));
 app.use(errorHandler);
 //app listen on port
 app.listen(PORT, () => {
